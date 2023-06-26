1f2ef969d07197b5f0517769f99d753e1bcab5d6
FriarobbyCollege <ballourl@mail.uc.edu>
2023-06-26 18:32:53 -0400

    Lab 7
    ALb 7 Work

---
+++ Lab07/src/PartA.java
@@ -0,0 +1,32 @@
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+
+public class PartA {
+
+    public static void main(String[] args) {
+
+        for(int j = 0; j < 31; j++)
+        {
+            System.out.println(j);
+        };
+        System.out.println();
+
+        for(int i = 30; i > -1; i--)
+        {
+            System.out.println(i);
+        };
+        System.out.println();
+
+        for(int k = 0; k <= 18; k+=3)
+        {
+            System.out.println(k);
+        };
+        System.out.println();
+
+        for(int l = 10; l >-1; l-=2)
+        {
+            System.out.println(l);
+        };
+        System.out.println();
+
+    }
+}
